80a939ca4907bb47d406fb9f2b9fa5f95e40cf63
spiritphyz <heytonyle@icloud.com>
2016-10-01 10:59:52 -0700

    Implement live search, letter-by-letter

--- src/index.js
+++ src/index.js
@@ -1,2 +1,2 @@
 // TODO: Render the `App` component to the DOM
-ReactDOM.render(<App video={exampleVideoData[0]} search={searchYouTube}/>, document.getElementById("app"));
+ReactDOM.render(<App video={exampleVideoData[0]} search={searchYouTube}/>, document.getElementById('app'));
